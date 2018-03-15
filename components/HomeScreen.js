9efd6cf1d7ef213d7a6b203e25b33f5f0bea4bb5
Philipp Brumm <philipp.brumm@gmail.com>
2018-03-15 20:19:55 +0100

    mache HomeScreen inhalt scrollable

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, ScrollView } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
 import Header from './Header';
@@ -16,38 +16,43 @@ export default class HomeScreen extends React.Component {
     return (
       <Fragment>
         <Header title={`Vertretungen - ${grade}`} navigation={this.props.navigation} />
-        <View style={{ flex: 1 }}>
-          {actualSubstitutes ? (
-            <List containerStyle={{ marginBottom: 20 }}>
-              {actualSubstitutes.map(({ teacher, lesson, isFree, subject }, index) => (
-                <ListItem
-                  containerStyle={{
-                    backgroundColor: isFree ? 'rgb(27, 159, 44)' : undefined,
-                  }}
-                  titleStyle={{
-                    color: !isFree ? 'orange' : 'black',
-                  }}
-                  rightTitleStyle={{
-                    color: !isFree ? 'orange' : 'black',
-                  }}
-                  subtitleStyle={{
-                    color: !isFree ? 'orange' : 'black',
-                  }}
-                  hideChevron={true}
-                  key={index}
-                  title={subject}
-                  subtitle={teacher}
-                  rightTitle={lesson}
-                />
-              ))}
-            </List>
-          ) : (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-              <Text style={{ fontSize: 100, marginBottom: 20 }}>😭</Text>
-              <Text>Keine Vertretungen für die {grade}</Text>
-            </View>
-          )}
-        </View>
+        <ScrollView contentContainerStyle={{ flex: 1 }}>
+          <View style={{ flex: 1 }}>
+            {actualSubstitutes ? (
+              <List containerStyle={{ marginBottom: 20 }}>
+                {actualSubstitutes.map(({ teacher, lesson, isFree, subject, room }, index) => {
+                  return (
+                    <ListItem
+                      containerStyle={{
+                        backgroundColor: isFree ? '#a5d794' : undefined,
+                        borderBottomColor: isFree ? '#68a753' : '#bbb',
+                      }}
+                      titleStyle={{
+                        width: 300,
+                      }}
+                      subtitleStyle={{
+                        width: 300,
+                      }}
+                      rightTitleStyle={{
+                        color: isFree ? '#3e702d' : '#bbb',
+                      }}
+                      hideChevron
+                      key={index}
+                      title={`Fach: ${subject} ${room ? `Raum: ${room}` : ''}`}
+                      subtitle={teacher && `Lehrer: ${teacher}`}
+                      rightTitle={lesson}
+                    />
+                  );
+                })}
+              </List>
+            ) : (
+              <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text style={{ fontSize: 100, marginBottom: 20 }}>😭</Text>
+                <Text>Keine Vertretungen für die {grade}</Text>
+              </View>
+            )}
+          </View>
+        </ScrollView>
       </Fragment>
     );
   }
