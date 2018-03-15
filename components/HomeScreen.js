c894ca6970040b312c53485c1cd79cf98c82b9df
Philipp Brumm <philipp.brumm@gmail.com>
2018-03-15 19:39:40 +0100

    eine ausgwählte klasse kann an diesem tag auch keine vertretungen haben (boo!)
    ...also zeige eine informative view

--- components/HomeScreen.js
+++ components/HomeScreen.js
@@ -1,5 +1,5 @@
 import React, { Fragment } from 'react';
-import { View } from 'react-native';
+import { View, Text } from 'react-native';
 import { List, ListItem } from 'react-native-elements';
 
 import Header from './Header';
@@ -17,29 +17,36 @@ export default class HomeScreen extends React.Component {
       <Fragment>
         <Header title={`Vertretungen - ${grade}`} navigation={this.props.navigation} />
         <View style={{ flex: 1 }}>
-          <List containerStyle={{ marginBottom: 20 }}>
-            {actualSubstitutes.map(({ teacher, lesson, isFree, subject }, index) => (
-              <ListItem
-                containerStyle={{
-                  backgroundColor: isFree ? 'rgb(27, 159, 44)' : undefined,
-                }}
-                titleStyle={{
-                  color: !isFree ? 'orange' : 'black',
-                }}
-                rightTitleStyle={{
-                  color: !isFree ? 'orange' : 'black',
-                }}
-                subtitleStyle={{
-                  color: !isFree ? 'orange' : 'black',
-                }}
-                hideChevron={true}
-                key={index}
-                title={subject}
-                subtitle={teacher}
-                rightTitle={lesson}
-              />
-            ))}
-          </List>
+          {actualSubstitutes ? (
+            <List containerStyle={{ marginBottom: 20 }}>
+              {actualSubstitutes.map(({ teacher, lesson, isFree, subject }, index) => (
+                <ListItem
+                  containerStyle={{
+                    backgroundColor: isFree ? 'rgb(27, 159, 44)' : undefined,
+                  }}
+                  titleStyle={{
+                    color: !isFree ? 'orange' : 'black',
+                  }}
+                  rightTitleStyle={{
+                    color: !isFree ? 'orange' : 'black',
+                  }}
+                  subtitleStyle={{
+                    color: !isFree ? 'orange' : 'black',
+                  }}
+                  hideChevron={true}
+                  key={index}
+                  title={subject}
+                  subtitle={teacher}
+                  rightTitle={lesson}
+                />
+              ))}
+            </List>
+          ) : (
+            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+              <Text style={{ fontSize: 100, marginBottom: 20 }}>😭</Text>
+              <Text>Keine Vertretungen für die {grade}</Text>
+            </View>
+          )}
         </View>
       </Fragment>
     );
