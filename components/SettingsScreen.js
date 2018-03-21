5febed9c602a56ef25b25b3593a528cc06ad3b7c
Michel Rothböck <mrothboeck@cimm-a-c-012.co-in.local>
2018-03-21 15:32:55 +0100

    Einstellungen für Push-Notifications

--- components/SettingsScreen.js
+++ components/SettingsScreen.js
@@ -1,28 +1,40 @@
-import React, { Fragment } from 'react';
-import { View, Platform } from 'react-native';
+import React, { Fragment, Component } from 'react';
+import { View, Text, Platform, StyleSheet, Switch } from 'react-native';
 import { Button } from 'react-native-elements';
 import PushNotification from 'react-native-push-notification';
 
 import Header from './Header';
-import PushControler from './PushHandler';
-
-sendMessage = text => {
-  PushNotification.localNotification({
-    message: text,
-    badge: 1,
-  });
-};
+import PushController from './PushHandler';
 
 export default class SettingsScreen extends React.Component {
+  state = { switch: true };
   static navigationOptions = {
     drawerLabel: 'Einstellungen',
+  };
+  sendMessage = text => {
+    if (this.state.switch) {
+      PushNotification.localNotification({
+        message: text,
+      });
+    }
+  };
+  ac_switch = () => {
+    this.setState({ switch: !this.state.switch });
   };
   render() {
     const { screenProps: { reloadData } } = this.props;
     return (
       <Fragment>
-        <PushControler />
+        <PushController />
         <Header title="Einstellungen" navigation={this.props.navigation} />
+        <View style={{ backgroundColor: 'grey' }}>
+          <View style={{ flexDirection: 'row', margin: 20 }}>
+            <Text style={{ fontSize: 16, color: 'white' }}>Push Nachrichtenhten</Text>
+            <View style={{ flex: 1, alignItems: 'flex-end' }}>
+              <Switch onValueChange={this.ac_switch} value={this.state.switch} />
+            </View>
+          </View>
+        </View>
         <View
           style={{ flex: 1, alignItems: 'center', justifyContent: 'flex-end', paddingBottom: 50 }}
         >
@@ -37,7 +49,7 @@ export default class SettingsScreen extends React.Component {
               marginBottom: 20,
             }}
             title="Message Test"
-            onPress={() => sendMessage('test')}
+            onPress={() => this.sendMessage('test')}
           />
           <Button
             buttonStyle={{
